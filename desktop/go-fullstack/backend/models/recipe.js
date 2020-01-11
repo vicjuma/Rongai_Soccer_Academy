10f9c2a90a8501c696c12aa16cd2a458501baea9
victor juma <vicjuma945@gmail.com>
2020-01-11 16:38:20 +0300

    debugging

--- desktop/go-fullstack/backend/models/recipe.js
+++ desktop/go-fullstack/backend/models/recipe.js
@@ -4,7 +4,7 @@ const recipeSchema = mongoose.Schema({
     title: { type: String, required: true },
     ingredients: { type: String, required: true },
     instructions: { type: String, required: true },
-    time: { type: String, required: true },
+    time: { type: Number, required: true },
     difficulty: { type: Number, required: true },
 });
 
